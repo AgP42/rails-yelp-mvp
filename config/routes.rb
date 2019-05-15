efe9073c85ae58de4d11c840b7a06acccb1858ae
Agathe Pinel <agp.com@mailbox.org>
2019-05-15 18:49:27 +0200

    restaurant multi model done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  # detailed version :
+  root to: 'restaurants#index'
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :review
+  post 'restaurants/:restaurant_id/reviews/', to: 'reviews#create', as: :restaurant_reviews
+
+  # #shortcut :
+  # resources :restaurants do
+  #   resources :reviews, only: [ :new, :create ]
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '+ 33 6 78 87 777',
+    category:      'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, Berlin E1 6PQ',
+    phone_number:  '0156349856',
+    category:       'italian'
+  },
+  {
+    name:         'Mustafa',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '+447639',
+    category:       'belgian'
+  },
+  {
+    name:         'Vin pearl',
+    address:      '56A Shoreditch High St, Berlin E1 6PQ',
+    phone_number:  '06665645656567',
+    category:        'japanese'
+  },
+  {
+    name:         'Imnu',
+    address:      '56A Shoreditch High St, Paris E1 6PQ',
+    phone_number:  '05 67 65 55 54',
+    category:        'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
